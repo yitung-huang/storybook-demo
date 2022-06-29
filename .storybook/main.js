69cf475483dfd664447412e860d316e712fd2f7b
Thomas Huang <thomas.huang@aligent.com.au>
2022-06-29 15:47:53 +0930

    Include story book add ons

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,10 +2,21 @@ module.exports = {
 	stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
 	staticDirs: ["../public"],
 	addons: [
+		// Default add ons
 		"@storybook/addon-links",
 		"@storybook/addon-essentials",
 		"@storybook/preset-create-react-app",
-		"@storybook/addon-interactions"
+		"@storybook/addon-interactions",
+
+		// Added add ons
+		"@storybook/addon-a11y",
+		"@storybook/addon-backgrounds",
+		"@storybook/addon-docs",
+		"@storybook/addon-measure",
+		"@storybook/addon-outline",
+		"@storybook/addon-toolbars",
+		"@storybook/addon-viewport",
+		"storybook-addon-pseudo-states"
 	],
 	features: {
 		postcss: false
