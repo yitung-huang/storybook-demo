2710a15b68373d803556e08e1678bdd6f1a78d36
Thomas Huang <thomas.huang@aligent.com.au>
2022-08-04 11:12:22 +0930

    Implement testing on storybook with login button
    Added hover styling to buttons so we can see the colour change when we test the button click.

--- src/components/Header/Header.stories.jsx
+++ src/components/Header/Header.stories.jsx
@@ -3,10 +3,16 @@ import React from "react";
 import { Greeting } from "./Greeting";
 import { Header } from "./Header";
 
+import { expect } from "@storybook/jest";
+import { userEvent, waitFor, within } from "@storybook/testing-library";
+
 export default {
   title: "Example/Header",
   component: Header,
-  subcomponents: { Greeting }
+  subcomponents: { Greeting },
+  argTypes: {
+    onLogout: { action: true }
+  }
 };
 
 const Template = (args) => <Header {...args} />;
@@ -17,6 +23,12 @@ LoggedIn.args = {
     name: "Hummus"
   }
 };
+LoggedIn.play = async ({ args, canvasElement }) => {
+  const canvas = within(canvasElement);
+
+  await userEvent.click(canvas.getByRole("button"));
+
+  await waitFor(() => expect(args.onLogout).toHaveBeenCalled());
 };
 
 export const LoggedOut = Template.bind({});
