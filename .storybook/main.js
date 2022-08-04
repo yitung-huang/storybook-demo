021f39080f84e0771242c17048c754fe66dce125
Thomas Huang <thomas.huang@aligent.com.au>
2022-08-04 11:08:16 +0930

    Set up to load MDX files and create a basic MDX version for ProgressIndicator

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,8 @@
 module.exports = {
-	stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+	stories: [
+		"../src/**/*.stories.mdx",
+		"../src/**/*.stories.@(js|jsx|ts|tsx|mdx)"
+	],
 	staticDirs: ["../public"],
 	addons: [
 		// Default add ons
